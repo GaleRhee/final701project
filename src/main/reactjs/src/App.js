af46cf978b7a0c320ebc997a94270bbb5d188d53
GaleRhee <jun5hero@naver.com>
2024-01-26 17:51:15 +0900

    successgit add .

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -7,7 +7,7 @@ function App(props) {
     <div className='App'>
       <img alt='' src={mainImg} />
       <h2><b>스프링 부트 + 리액트 CI/CD 성공 기원!!!<br />한번에 성공하기</b></h2>
-      <h3>끝까지 남았다!!</h3>
+      <h3>끝까지 남았다!!!</h3>
     </div>
   );
 }

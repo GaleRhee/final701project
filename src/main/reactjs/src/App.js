b6270adc89e5ef8feb1c43fc8dd962780b89bcf9
GaleRhee <jun5hero@naver.com>
2024-01-26 18:01:09 +0900

    error solved

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -8,6 +8,7 @@ function App(props) {
       <img alt='' src={mainImg} />
       <h2><b>스프링 부트 + 리액트 CI/CD 성공 기원!!!<br />한번에 성공하기</b></h2>
       <h3>끝까지 남았다!!!</h3>
+      <h4>그런가?</h4>
     </div>
   );
 }
